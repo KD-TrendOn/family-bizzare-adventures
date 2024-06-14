874c0c099be4d1d2c6d3d70281cb0c23234afb22
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-06-14 11:52:22 +0300

    connect routers

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .storys.views import router as story_router
-
+from .nodes.views import router as node_router
 router = APIRouter()
 router.include_router(router=story_router, prefix="/storys")
+router.include_router(router=node_router, prefix="/nodes")
