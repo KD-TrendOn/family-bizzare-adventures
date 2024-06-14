b2de815d196345ad766fe40407a41e891f444688
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-06-14 10:10:35 +0300

    added get_node_instance

--- api_v1/nodes/crud.py
+++ api_v1/nodes/crud.py
@@ -9,7 +9,7 @@ from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Node, Story
-
+from typing import Optional
 from .schemas import NodeCreate, NodeBase
 
 async def get_nodes_of_story(session: AsyncSession, story_id: int) -> list[Node]:
@@ -33,3 +33,11 @@ async def create_node(session: AsyncSession, node_in: NodeBase) -> Node:
     return node
 
 
+async def get_node_instance(session: AsyncSession, path_id: int, parent_id: int, story_id:int) -> Optional[Node]:
+    stmt = select(Node).where(Node.story_id == story_id and Node.path_id == path_id, Node.parent_id == parent_id).order_by(Node.id)
+    result: Result = await session.execute(stmt)
+    node = result.scalars().all()
+    nodes = list(node)
+    if len(nodes) == 0:
+        return None
+    return nodes[0]
